daa304b60be5af5ebd89491c7fe87b6b0f49ca1b
TonyStar123 <www.tonyxiao@gmail.com>
2024-04-18 20:10:16 -0400

    fix

---
+++ Practicum 15/arithmetic-services/src/arithmetica.js
@@ -0,0 +1,20 @@
+function add(n, m){
+    return n + m;
+}
+
+function subtract(n, m){
+    return n - m;
+}
+
+function multiply(n, m){
+    return n * m;
+}
+
+function divide(n, m){
+    return n / m;
+}
+
+module.exports = { add };
+module.exports = { subtract };
+module.exports = { multiply };
+module.exports = { divide };
